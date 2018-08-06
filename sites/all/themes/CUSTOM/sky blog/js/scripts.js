9eaf4cc1278adcc62b5bd6948b3efa620d6d7098
skywalks-dev-team <vishnukunnuveettil@gmail.com>
2018-08-06 21:30:12 +0530

    test

---
+++ sites/all/themes/CUSTOM/sky blog/js/scripts.js
@@ -0,0 +1,14 @@
+// We define a function that takes one parameter named $.
+(function ($) {
+  // Use jQuery with the shortcut:
+  // console.log($.browser);
+  // Here we immediately call the function with jQuery as the parameter.
+  jQuery(document).ready(function($) {
+  // Code that uses jQuery's $ can follow here.
+//  	$('input[type=text]').addClass('form-control mr-sm-2');
+//  	$('input[type=search]').addClass('form-control mr-sm-2');
+//  	$('input[type=password]').addClass('form-control mr-sm-2');
+//  	$('input[type=mail]').addClass('form-control mr-sm-2');
+//  	$('input[type=submit]').addClass('btn btn-secondary');
+	});
+}(jQuery));
