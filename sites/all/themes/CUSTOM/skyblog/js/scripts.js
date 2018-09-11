0eb94164e345dda95eb8e826e9608c5a9f259a6a
skywalks-dev-team <vishnukunnuveettil@gmail.com>
2018-09-11 21:39:57 +0530

    test

--- sites/all/themes/CUSTOM/skyblog/js/scripts.js
+++ sites/all/themes/CUSTOM/skyblog/js/scripts.js
@@ -8,8 +8,9 @@
   		var test = $('.dash-menu').width();
   		if (test > 100) {
   			$('.dash-menu').css("width", "55px");
-  			$(".level-1").children("a").css("color","transparent");
-  			$(".level-1").children("a").css("overflow","hidden");
+        $("li.tb-megamenu-item.level-2.mega").children("a").css("color","transparent");
+  			$("li.tb-megamenu-item.level-2.mega").children("i").css("color","#fff");
+        $(".level-1").children("a").css("overflow","hidden");
   			$(".svg-inline--fa").css("color","#fff");
   			// $('.level-1').children("a.dropdown-toggle").css("height", "10px");
   			$('.level-1').children("a.dropdown-toggle").css("width", "55px");
@@ -17,13 +18,13 @@
   			$('.level-1').children("a").css("height", "45px");
 
   		}else{
-  			$('.dash-menu').css("width", "");
-  			$(".level-1").children("a").css("color","unset");
-        $(".level-2").children("a").css("color","initial");
-  			$('.level-1').children("a.dropdown-toggle").css("width", "initial");
-  			$('.level-1').children("a.dropdown-toggle").css("height", "initial");
-  			$('.level-1').children("a").css("height", "initial");
-  		}
+        $('.dash-menu').css("width", "initial");
+        $("li.tb-megamenu-item.level-1.mega.tb-3.mega-align-left.dropdown").children("a").css("color","#fff");
+        $("li.tb-megamenu-item.level-2.mega").children("a").css("color","#fff");
+        $('.level-1').children("a.dropdown-toggle").css("width", "270px");
+        $('.level-1').children("a.dropdown-toggle").css("height", "65px");
+        
+      }
   	})
 	});
 }(jQuery));
--- sites/all/themes/CUSTOM/skyblog/template.php
+++ sites/all/themes/CUSTOM/skyblog/template.php
@@ -6,10 +6,10 @@ function skyblog_preprocess_page(&$variables) {
 	if (($user->uid != 1) && ($user->uid != 5)) {
 		$css = drupal_get_path('module', 'sky_dashboard')."/css/not-admin.css";
 		drupal_add_css($css,array('group' => CSS_DEFAULT, 'every_page' => TRUE));
-		pr(drupal_add_css($css,array('group' => CSS_DEFAULT, 'every_page' => TRUE)));
+		// pr(drupal_add_css($css,array('group' => CSS_DEFAULT, 'every_page' => TRUE)));
 	}
 	else{
-		pr('test2');
+		// pr('test2');
 		return true;
 		
 	}
